d8450c71bf9368c6ae3234c945c0fa0dbd5583ef
yuricode1014 <yuriko19871014@gmail.com>
2022-08-25 14:40:53 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.static(path.resolve(__dirname, "/build")));
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log("App listening on port " + PORT);
+});
